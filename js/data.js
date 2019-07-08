52be04faa7430e185a8c5f4e0db2b1ac2aa6ecf3
Rimantas <belauzas@yahoo.com>
2019-07-08 10:46:47 +0300

    Footer icons

--- js/data.js
+++ js/data.js
@@ -1,1 +1,20 @@
 "use strict";
+
+const footerIcons = [
+    {
+        icon: 'facebook',
+        link: '#'
+    },
+    {
+        icon: 'twitter',
+        link: '#'
+    },
+    {
+        icon: 'dribbble',
+        link: '#'
+    },
+    {
+        icon: 'behance',
+        link: '#'
+    }
+];
--- js/functions.js
+++ js/functions.js
@@ -1,1 +1,18 @@
 "use strict";
+
+function generateFooterIcons( data ) {
+    let HTML = '';
+
+    if ( !Array.isArray(data) ) {
+        console.error('Wrong data type. Has to be an array.');
+        return;
+    }
+
+    for ( let i=0; i<data.length; i++ ) {
+        HTML += `<a class="asd" href="${data[i].link}">
+                    <i class="fa fa-${data[i].icon}"></i>
+                </a>`;
+    }
+
+    return HTML;
+}
